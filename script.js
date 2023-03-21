5e5be319d01cca6fa774599361021d0dd0e65a2b
Valentín Gartner <DEvalen200@gmail.com>
2023-03-21 23:47:12 +0100

    Win Lose available

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ var maxTurn = 10;
 var currentDraggable;
 
 var currentRandomNumber;
+const numbersList = Array(maxTurn)//[-1000,-1000,-1000,-1000,-1000,-1000,-1000,-1000,-1000,-1000,-1000,-1000,-1000,-1000,-1000,-1000,-1000,-1000,-1000,-1000,];
+
+for (index = 0; index < numbersList.length; index++) {
+    numbersList[index] = -1000;
+}
 
 currentDraggable = document.getElementById("draggable_0");
 currentRandomNumber = getRandomInt(1000);
@@ -15,13 +20,121 @@ function reload()
 {
     window.location.reload();
 }
+
 function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+    var temp = Math.floor(Math.random() * max);
+    while(numbersList.includes(temp))
+    {
+        temp = Math.floor(Math.random() * max);
+    }
+
+
+    //Check possible positions
+    checkPossiblePositions(temp);
+    if (checkIfLose())
+    {
+        console.log("You Lost");
+    }
+    else
+    {
+        currentDraggable.draggable = true;
+    }
+    
+    return temp;
   }
+
+function checkIfLose()
+{
+    //If possible positions = 0;
+
+    var possibleOptions = new Array();
+
+    for (let index = 0; index < containters.length; index++) {
+        if (containters[index].classList.contains("posibleOption"))
+        {
+            possibleOptions.push(containters[index]);
+        }
+        
+    }
+
+    if (possibleOptions.length > 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+
+}
+
+function checkPossiblePositions(_temp)
+{
+    if (currentTurn == 0)
+    {
+        return;
+    }
+    var tempNumber = -1000;
+    for (let index = 0; index < numbersList.length; index++) {
+        if (numbersList[index] > _temp)
+        {
+            tempNumber = numbersList[index];
+            break;
+        }
+    }
+
+    var lastNumberInListIndex = -1000;
+    if (tempNumber == -1000)
+    {
+        for (let index = 0; index < numbersList.length; index++) {
+            if (numbersList[index] != -1000)
+            {
+                lastNumberInListIndex = index;
+            }
+        }
+
+        for (let index = 0; index < containters.length; index++) {
+            if (index > lastNumberInListIndex)
+            {
+                containters[index].classList.add("posibleOption");
+            }
+        }
+    }
+    else //Hay algún número mayor que currentNumber
+    {
+        var tempIndex = numbersList.indexOf(tempNumber);
+    
+        for (let index = 9; index < numbersList.length; index--) {
+            if (index < tempIndex)
+            {
+                if (numbersList[index] == -1000)
+                {
+                    containters[index].classList.add("posibleOption");
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+    }
+
+}
 
 draggables.forEach(draggable =>{
     draggable.addEventListener("dragstart", () =>{
         draggable.classList.add("dragging");
+    })
+
+    draggable.addEventListener("click", () =>{
+        if (draggable.classList.contains("noNumber"))
+        {
+            draggable.classList.remove("noNumber");
+            currentRandomNumber = getRandomInt(1000);
+            currentDraggable.innerHTML = currentRandomNumber.toString();
+        }
+
     })
 
     draggable.addEventListener("dragend", () =>{
@@ -41,14 +154,29 @@ draggables.forEach(draggable =>{
                     if (currentTurn >= maxTurn)
                     {
                         //Win
+                        console.log("You Win");
                     }
                     else
                     {
                         var nextDraggable = document.getElementById("draggable_" + currentTurn.toString());
                         nextDraggable.classList.remove("hidden");
                         currentDraggable = nextDraggable;
-                        currentRandomNumber = getRandomInt(1000);
-                        currentDraggable.innerHTML = currentRandomNumber.toString();
+
+                        currentDraggable.draggable = false;
+                        numbersList[parseInt(containter.id)] = currentRandomNumber;
+
+                        //Remove PosibleOption
+                        for (let index = 0; index < containters.length; index++) {
+                            if (containters[index].classList.contains("posibleOption"))
+                            {
+                                containters[index].classList.remove("posibleOption");
+                            }
+                        }
+
+                        //Generate Next Number
+                        /*currentRandomNumber = getRandomInt(1000);
+                        currentDraggable.innerHTML = currentRandomNumber.toString();*/
+                        console.log(numbersList);
                     }
                 }
             })      
